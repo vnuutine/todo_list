9633656aee2b475bc9cbf1df9ba841dfd6bc8998
Ville Nuutinen <vnuutinen1@hotmail.com>
2025-12-25 19:56:51 +0200

    feat: function to write tasks to storage
    Add a save_all_tasks function to write tasks to the storage file. Also refactors load_all_tasks for improved readability.

--- todo_list/storage.py
+++ todo_list/storage.py
@@ -4,13 +4,15 @@ import json
 path_location = Path(r"/Users/ville/Documents/Projects/Python/todo_list/data/storage.json")
 
 def load_all_tasks():
-    path_exists = path_location.exists()
-    
-    if path_exists == False:
+    if not path_location.exists():
         with open(path_location, "x") as r:
             json.dump([], r)
             return []
-    
     else:
         with open(path_location) as f:
             return json.load(f)
+        
+
+def save_all_tasks(tasks):
+    with open(path_location, "w") as f:
+        json.dump(tasks, f)
